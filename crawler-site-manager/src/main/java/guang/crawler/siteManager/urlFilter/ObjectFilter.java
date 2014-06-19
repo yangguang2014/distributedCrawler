c8a7916ffd36b9993553b455a53c5c7e316ce723
yang <yang@yang-PC>
2014-06-19 21:51:41 +0800

    添加了注释

--- crawler-site-manager/src/main/java/guang/crawler/siteManager/urlFilter/ObjectFilter.java
+++ crawler-site-manager/src/main/java/guang/crawler/siteManager/urlFilter/ObjectFilter.java
@@ -2,13 +2,27 @@ package guang.crawler.siteManager.urlFilter;
 
 public interface ObjectFilter
 {
-	
+	/**
+	 * 检查是否包含某个对象
+	 * @param object
+	 * @return
+	 */
 	public boolean contains(Object object);
-	
+	/**
+	 * 检查是否包含某个对象，如果没有包含，那么添加该对象
+	 * @param object
+	 * @return
+	 */
 	public boolean containsAndSet(Object object);
-	
+	/**
+	 * 从备份字符串中恢复过滤器
+	 * @param data
+	 */
 	public void fromBackupString(String data);
-	
+	/**
+	 * 将当前对象序列化为一个字符串
+	 * @return
+	 */
 	public String toBackupString();
 	
 }
